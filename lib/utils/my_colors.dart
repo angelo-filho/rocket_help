d21e3757be20394496ee33522eabbee6cd8d43d3
Angelo Filho <angelo.filho06@hotmail.com>
2023-06-29 20:21:27 -0300

    feat: create my colors class

---
+++ lib/utils/my_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static final Map<int, Color> gray = {
+    100: const Color(0xFFE1E1E6),
+    200: const Color(0xFFC4C4CC),
+    300: const Color(0xFF7C7C8A),
+    400: const Color(0xFF323238),
+    500: const Color(0xFF29292E),
+    600: const Color(0xFF202024),
+    700: const Color(0xFF121214),
+  };
+
+  static final Map<int, Color> green = {
+    300: const Color(0xFF04D361),
+    500: const Color(0xFF00B37E),
+    700: const Color(0xFF00875F),
+  };
+
+  static const Color primary = Color(0xFF996DFF);
+
+  static const Color secondary = Color(0xFFFBA94C);
+}
