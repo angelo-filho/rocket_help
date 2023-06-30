2b958a13b16c3403dd12937c2d74fba804ce634f
Angelo Filho <angelo.filho06@hotmail.com>
2023-06-30 20:46:06 -0300

    feat: add tab section widget to home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:rocket_help/utils/my_colors.dart';
 
-import '../widgets/finished_tab.dart';
-import '../widgets/on_going_tab.dart';
+import '../widgets/tab_section.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -58,47 +57,7 @@ class HomeScreen extends StatelessWidget {
               ],
             ),
             const SizedBox(height: 32),
-            DefaultTabController(
-              length: 2,
-              initialIndex: 0,
-              child: Expanded(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 35,
-                      child: TabBar(
-                        labelColor: MyColors.secondary,
-                        unselectedLabelColor: MyColors.gray[300],
-                        indicator: BoxDecoration(
-                          borderRadius: BorderRadius.circular(4),
-                          border: Border.all(color: MyColors.secondary),
-                          color: MyColors.gray[600],
-                        ),
-                        tabs: const [
-                          Tab(
-                            text: "Em andamento",
-                          ),
-                          Tab(
-                            text: "Finalizado",
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 32,
-                    ),
-                    const Expanded(
-                      child: TabBarView(
-                        children: [
-                          OnGoingTab(),
-                          FinishedTab(),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            const HomeScreenTabSection(),
             ElevatedButton(
               onPressed: () => {},
               style: ElevatedButton.styleFrom(
