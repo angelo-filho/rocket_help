04817aefa8b4976582eadbeab9669aaae24bbad0
Angelo Filho <angelo.filho06@hotmail.com>
2023-07-02 23:57:36 -0300

    feat: add solicitation app bar widget to new solicitation screen

--- lib/screens/new_solicitation_screen.dart
+++ lib/screens/new_solicitation_screen.dart
@@ -1,35 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:rocket_help/utils/my_colors.dart';
 import 'package:rocket_help/widgets/dark_text_field.dart';
 import 'package:rocket_help/widgets/primary_button.dart';
+import 'package:rocket_help/widgets/solicitation_app_bar.dart';
 
 class NewSolicitationScreen extends StatelessWidget {
   const NewSolicitationScreen({super.key});
-
-  void _handleBackToHome(BuildContext context) {
-    Navigator.pop(context);
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MyColors.gray[600],
-      appBar: AppBar(
-        backgroundColor: MyColors.gray[600],
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(
-            PhosphorIcons.regular.caretLeft,
-            color: Colors.white,
-          ),
-          onPressed: () => _handleBackToHome(context),
-        ),
-        title: const Text("Solicitação"),
-        titleTextStyle: const TextStyle(
-            color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-        centerTitle: true,
-      ),
+      appBar: const SolicitationAppBar(),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(32, 16, 32, 0),
         child: SingleChildScrollView(
