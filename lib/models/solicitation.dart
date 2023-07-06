8f4666f60f792e79e9db56287b04acd87409f6c6
Angelo Filho <angelo.filho06@hotmail.com>
2023-07-06 16:41:40 -0300

    feat: create solicitation model

---
+++ lib/models/solicitation.dart
@@ -0,0 +1,21 @@
+class Solicitation {
+  Solicitation({
+    required this.equipment,
+    required this.description,
+    required this.dateOfRegister,
+  })  : isFinished = false,
+        solution = null;
+
+  Solicitation.withStatusFinished({
+    required this.equipment,
+    required this.description,
+    required this.dateOfRegister,
+    required this.solution,
+  }) : isFinished = true;
+
+  final String equipment;
+  final String description;
+  final DateTime dateOfRegister;
+  final bool isFinished;
+  final String? solution;
+}
