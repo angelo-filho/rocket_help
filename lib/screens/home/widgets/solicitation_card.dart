3ab02e44346c183808f4f1528e0f203195edb94d
Angelo Filho <angelo.filho06@hotmail.com>
2023-07-06 18:14:37 -0300

    feat: add action to navigate in solicitation card
    - Solicitations cards can now navigate to correspond solicitation details screen

--- lib/screens/home/widgets/solicitation_card.dart
+++ lib/screens/home/widgets/solicitation_card.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:rocket_help/screens/solicitation_details/solicitation_details_screen.dart';
 
 import '../../../utils/my_colors.dart';
 
 class SolicitationCard extends StatelessWidget {
-  const SolicitationCard({super.key, required this.isFinished});
+  const SolicitationCard({
+    super.key,
+    required this.isFinished,
+    required this.index,
+  });
 
   final bool isFinished;
+  final int index;
+
+  void goToSolicitationDetails(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) =>
+              SolicitationDetailsScreen(solicitationIndex: index),
+        ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,70 +30,73 @@ class SolicitationCard extends StatelessWidget {
         ? PhosphorIcons.regular.sealCheck
         : PhosphorIcons.regular.clockAfternoon;
 
-    return Container(
-      width: double.infinity,
-      height: 88,
-      padding: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: MyColors.gray[600],
-        borderRadius: BorderRadius.circular(5),
-        border: Border(
-          left: BorderSide(color: mainColor, width: 8),
-          right: BorderSide(color: mainColor, width: 0),
-          top: BorderSide(color: mainColor, width: 0),
-          bottom: BorderSide(color: mainColor, width: 0),
+    return GestureDetector(
+      onTap: () => goToSolicitationDetails(context),
+      child: Container(
+        width: double.infinity,
+        height: 88,
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: MyColors.gray[600],
+          borderRadius: BorderRadius.circular(5),
+          border: Border(
+            left: BorderSide(color: mainColor, width: 8),
+            right: BorderSide(color: mainColor, width: 0),
+            top: BorderSide(color: mainColor, width: 0),
+            bottom: BorderSide(color: mainColor, width: 0),
+          ),
         ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Patrimônio 147456",
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  fontSize: 16,
-                  color: Colors.white,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Patrimônio 147456",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 4),
-              Row(
-                children: [
-                  Icon(
-                    PhosphorIcons.regular.clockAfternoon,
-                    size: 16,
-                    color: MyColors.gray[300],
-                  ),
-                  const SizedBox(
-                    width: 4,
-                  ),
-                  Text(
-                    "20/01/22 às 14h",
-                    style: TextStyle(
-                      fontSize: 12,
+                const SizedBox(height: 4),
+                Row(
+                  children: [
+                    Icon(
+                      PhosphorIcons.regular.clockAfternoon,
+                      size: 16,
                       color: MyColors.gray[300],
                     ),
-                  ),
-                ],
-              )
-            ],
-          ),
-          Container(
-            width: 48,
-            height: 48,
-            decoration: BoxDecoration(
-              color: MyColors.gray[500],
-              shape: BoxShape.circle,
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    Text(
+                      "20/01/22 às 14h",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: MyColors.gray[300],
+                      ),
+                    ),
+                  ],
+                )
+              ],
             ),
-            child: Icon(
-              icon,
-              color: mainColor,
-            ),
-          )
-        ],
+            Container(
+              width: 48,
+              height: 48,
+              decoration: BoxDecoration(
+                color: MyColors.gray[500],
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                icon,
+                color: mainColor,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
