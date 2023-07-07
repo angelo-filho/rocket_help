1af66ffc293a6bfe9ff9683be0af1e5de343b57c
Angelo Filho <angelo.filho06@hotmail.com>
2023-07-07 12:40:34 -0300

    feat: controller property to dark text field widget

--- lib/widgets/dark_text_field.dart
+++ lib/widgets/dark_text_field.dart
@@ -6,15 +6,18 @@ class DarkTextField extends StatelessWidget {
     required this.hintText,
     this.prefixIcon,
     this.maxLines = 1,
+    this.controller,
   });
 
   final String hintText;
   final int maxLines;
   final Widget? prefixIcon;
+  final TextEditingController? controller;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: controller,
       style: const TextStyle(color: Colors.white, height: 1.5),
       decoration: InputDecoration(
         hintText: hintText,
