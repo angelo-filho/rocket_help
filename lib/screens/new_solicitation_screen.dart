38b51e1cd9b2a00b15ab9a8477b6e41ccd81550a
Angelo Filho <angelo.filho06@hotmail.com>
2023-07-07 14:29:30 -0300

    feat: add reactivity to new solicitation screen

--- lib/screens/new_solicitation_screen.dart
+++ lib/screens/new_solicitation_screen.dart
@@ -1,11 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:rocket_help/models/solicitation.dart';
+import 'package:rocket_help/models/solicitation_list_model.dart';
 import 'package:rocket_help/utils/my_colors.dart';
 import 'package:rocket_help/widgets/dark_text_field.dart';
 import 'package:rocket_help/widgets/primary_button.dart';
 import 'package:rocket_help/widgets/solicitation_app_bar.dart';
 
-class NewSolicitationScreen extends StatelessWidget {
+class NewSolicitationScreen extends StatefulWidget {
   const NewSolicitationScreen({super.key});
+
+  @override
+  State<NewSolicitationScreen> createState() => _NewSolicitationScreenState();
+}
+
+class _NewSolicitationScreenState extends State<NewSolicitationScreen> {
+  final _numberController = TextEditingController();
+  final _descriptionController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _numberController.dispose();
+    _descriptionController.dispose();
+  }
+
+  void addSolicitation() {
+    solicitations.add(
+      Solicitation(
+        equipment: _numberController.text,
+        description: _descriptionController.text,
+        dateOfRegister: DateTime.now(),
+      ),
+    );
+
+    Navigator.pop(context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,14 +46,17 @@ class NewSolicitationScreen extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              const DarkTextField(hintText: "Número do Patrimônio"),
+              DarkTextField(
+                  controller: _numberController,
+                  hintText: "Número do Patrimônio"),
               const SizedBox(height: 16),
-              const DarkTextField(
+              DarkTextField(
+                controller: _descriptionController,
                 hintText: "Descrição do problema",
                 maxLines: 18,
               ),
               const SizedBox(height: 16),
-              PrimaryButton(onTap: () {}, text: "Cadastrar"),
+              PrimaryButton(onTap: addSolicitation, text: "Cadastrar"),
               const SizedBox(height: 35),
             ],
           ),
