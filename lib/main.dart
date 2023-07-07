417e4809f7db6a7b6bfe0566d638c47f4b45b7f8
Angelo Filho <angelo.filho06@hotmail.com>
2023-07-07 14:56:18 -0300

    feat: initialize intl locale

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+import 'package:rocket_help/models/solicitation.dart';
+import 'package:rocket_help/models/solicitation_list_model.dart';
 
 import 'app.dart';
 
 void main() {
+  Intl.defaultLocale = "pt_BR";
+  initializeDateFormatting("pt_BR");
+
+  solicitations.add(
+    Solicitation(
+        equipment: "12345",
+        description: "Descrição top das galáxias",
+        dateOfRegister: DateTime.utc(2023, 3, 6, 13)),
+  );
+
+  solicitations.add(
+    Solicitation.withStatusFinished(
+        equipment: "54321",
+        description: "Deu ruim aqui menor",
+        dateOfRegister: DateTime.utc(2023, 4, 17, 8),
+        solution: "É complicado isso ai..."),
+  );
+
+  solicitations.add(
+    Solicitation(
+        equipment: "678910",
+        description: "A impressora não tá pegando!",
+        dateOfRegister: DateTime.utc(2023, 6, 1, 17)),
+  );
+
   runApp(const MyApp());
 }
