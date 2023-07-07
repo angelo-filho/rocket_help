f06cb324d5f000dc1db201cc513bf8584ea64d11
Angelo Filho <angelo.filho06@hotmail.com>
2023-07-07 17:19:19 -0300

    feat: add keyboard type to dark text field

--- lib/widgets/dark_text_field.dart
+++ lib/widgets/dark_text_field.dart
@@ -6,18 +6,21 @@ class DarkTextField extends StatelessWidget {
     required this.hintText,
     this.prefixIcon,
     this.maxLines = 1,
+    this.keyboardType,
     this.controller,
   });
 
   final String hintText;
   final int maxLines;
   final Widget? prefixIcon;
+  final TextInputType? keyboardType;
   final TextEditingController? controller;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: controller,
+      keyboardType: keyboardType,
       style: const TextStyle(color: Colors.white, height: 1.5),
       decoration: InputDecoration(
         hintText: hintText,
