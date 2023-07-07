9e78f9985b4429341bdc61e04e5d2f8b4bebd662
Angelo Filho <angelo.filho06@hotmail.com>
2023-07-07 16:30:38 -0300

    feat: add get solicitation by id method to solicitation list model

--- lib/models/solicitation_list_model.dart
+++ lib/models/solicitation_list_model.dart
@@ -5,14 +5,21 @@ class SolicitationListModel with ChangeNotifier {
   final List<Solicitation> _values = <Solicitation>[];
   List<Solicitation> get values => _values.toList();
 
+  Solicitation getSolicitationById(String id) {
+    return _values.singleWhere((solicitation) => solicitation.id == id);
+  }
+
   void add(Solicitation solicitation) {
     _values.add(solicitation);
     notifyListeners();
   }
 
-  void updateSolicitation(int index, String solution) {
-    _values[index].dateWhenFinished = DateTime.now();
-    _values[index].solution = solution;
+  void updateSolicitation(String id, String solution) {
+    var targetSolutionIndex =
+        _values.indexWhere((solicitation) => solicitation.id == id);
+
+    _values[targetSolutionIndex].dateWhenFinished = DateTime.now();
+    _values[targetSolutionIndex].solution = solution;
     notifyListeners();
   }
 }
