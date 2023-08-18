50395d9f114fa3b6e35ce1f11e5b17af1a40b1e9
Angelo Filho <angelo.filho06@hotmail.com>
2023-08-18 16:01:42 -0400

    feat: add dynamic message by filter type in solicitation list widget

--- lib/screens/home/widgets/solicitation_list.dart
+++ lib/screens/home/widgets/solicitation_list.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:rocket_help/models/solicitation.dart';
+import 'package:rocket_help/screens/home/home_screen.dart';
 import 'package:rocket_help/screens/home/widgets/empty_calls.dart';
 
 import 'solicitation_card.dart';
 
+const Map<SolicitationFilter, String> emptyErrorMessage = {
+  SolicitationFilter.none: "Você ainda não tem \n chamados criados",
+  SolicitationFilter.finished: "Você não tem \n chamados finalizados",
+  SolicitationFilter.notFinished: "Você não tem \n chamados em andamento",
+};
+
 class SolicitationsList extends StatelessWidget {
-  const SolicitationsList({super.key, required this.solicitations});
+  const SolicitationsList(
+      {super.key, required this.solicitations, required this.filterType});
 
   final List<Solicitation> solicitations;
+  final SolicitationFilter filterType;
 
   @override
   Widget build(BuildContext context) {
-    Widget mainWidget = const EmptyCalls();
+    Widget mainWidget = EmptyCalls(
+      message: emptyErrorMessage[filterType]!,
+    );
 
     if (solicitations.isNotEmpty) {
       mainWidget = ListView.builder(
